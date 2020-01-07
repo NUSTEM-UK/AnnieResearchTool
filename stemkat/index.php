a5642320478c3b4d5c21cf0ff34ef3a513820efa
Jonathan Sanderson <lists@quernstone.com>
2020-01-07 12:12:01 +0000

    Fix year selector.

--- stemkat/index.php
+++ stemkat/index.php
@@ -25,10 +25,10 @@
       <div class="jumbotron">
         <h1 class="display-4">STEMKAT: NUSTEM Games for CITE</h1>
         <p class="lead">Hello. Welcome to NUSTEM's games pages.</p>
-        <p>In a minute you will get to play two different games. But
-          before you get started please add your name, date of birth
+        <p>In a minute you will get to play two different games. <br />
+          Before you get started please add your name, date of birth
           and school name below (name and dates will be used to
-          create an anonymous identifier):</p>
+          create an anonymous identifier).</p>
       </div>
     </div>
     <div class="row">
@@ -107,23 +107,22 @@
             <div class="col">
               <select name="byear" class="form-control">
                 <option selected disabled>Year</option>
-                <option value="2000">2000</option>
-                <option value="2001">2001</option>
-                <option value="2002">2002</option>
-                <option value="2003">2003</option>
-                <option value="2004">2004</option>
-                <option value="2005">2005</option>
+                <option value="2015">2015</option>
+                <option value="2014">2014</option>
+                <option value="2013">2013</option>
+                <option value="2012">2012</option>
+                <option value="2011">2011</option>
+                <option value="2010">2010</option>
+                <option value="2009">2009</option>
+                <option value="2008">2008</option>
+                <option value="2007">2007</option>
                 <option value="2006">2006</option>
-                <option value-"2007">2007</option>
-                <option value-"2008">2008</option>
-                <option value-"2009">2009</option>
-                <option value-"2010">2010</option>
-                <option value-"2011">2011</option>
-                <option value-"2012">2012</option>
-                <option value-"2013">2013</option>
-                <option value-"2014">2014</option>
-                <option value-"2015">2015</option>
-                <option value-"2016">2016</option>
+                <option value="2005">2005</option>
+                <option value="2004">2004</option>
+                <option value="2003">2003</option>
+                <option value="2002">2002</option>
+                <option value="2001">2001</option>
+                <option value="2000">2000</option>
               </select>
             </div>
           </div>
