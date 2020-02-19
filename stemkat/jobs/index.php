c66bf5ecdeac47e15acb9f50ce3e93d0cc320daa
jjsanderson <jonathan.sanderson@northumbria.ac.uk>
2020-02-19 15:05:10 +0000

    interactJS to current

--- stemkat/jobs/index.php
+++ stemkat/jobs/index.php
@@ -103,7 +103,7 @@
     </div>
 	
     <!-- JS Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/interactjs@1.3.4/dist/interact.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/interactjs/dist/interact.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="src/artyom.window.min.js"></script>
     <script src="../src/shared.js"></script>
