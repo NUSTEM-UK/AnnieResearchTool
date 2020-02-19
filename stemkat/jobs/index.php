66bf07312e766358d36baae5ef4fce3601c0e2d5
jjsanderson <jonathan.sanderson@northumbria.ac.uk>
2020-02-19 15:04:39 +0000

    Update jQuery to 3.4.1

--- stemkat/jobs/index.php
+++ stemkat/jobs/index.php
@@ -104,7 +104,7 @@
 	
     <!-- JS Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/interactjs@1.3.4/dist/interact.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="src/artyom.window.min.js"></script>
     <script src="../src/shared.js"></script>
     <script src="src/index.js"></script>
